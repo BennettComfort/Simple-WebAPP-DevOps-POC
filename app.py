70f711d2606df06976969d6a68b928b45b18f61c
Bennett Comfort <comfort.bennett@gmail.com>
2019-12-28 15:27:37 -0600

    adding response headers to app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 # Import Dependencies
 from flask import Flask
+
 import uuid
+import os
+import re
 
 # Intialize the flask app
 app = Flask(__name__)
+
+@app.after_request
+def add_hostname_header(response):
+        env_host = str(os.environ.get('HOSTNAME'))
+        hostname = re.findall('[a-z]{3}-\d$', env_host)
+        if hostname:
+            response.headers["SP-LOCATION"] = hostname
+        return response
 
 @app.route('/')
 def get_uuid():
